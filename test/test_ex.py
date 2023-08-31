098ff43ea625548d11d5579ade36374bab2147e8
Annie Hernandez Perez <anniehperez@gmail.com>
2023-08-30 22:24:43 -0500

    checking test failure

--- test/test_ex.py
+++ test/test_ex.py
@@ -1,2 +1,11 @@
 def test_f_ex():
 	assert "hello"=="hello"
+
+def f_function_to_test(val):
+	return 123
+
+def test_f_function_to_test():
+	assert 123==f_function_to_test(123)
+
+	assert 124==f_function_to_test(123), "This is not 123"
+
