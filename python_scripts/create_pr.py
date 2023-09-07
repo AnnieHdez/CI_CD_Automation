fce16250a2ed8c0c5b2812edd93fe596a3e6a6db
Annie Hernandez Perez <anniehperez@gmail.com>
2023-09-07 18:09:28 -0500

    doing request and using token

--- python_scripts/create_pr.py
+++ python_scripts/create_pr.py
@@ -8,11 +8,15 @@ parser = argparse.ArgumentParser()
 
 # Adding Arguments
 parser.add_argument('--head_branch', type= str)
+parser.add_argument('--auth_token', type= str)
+
 
 args = parser.parse_args()
-AUTH_TOKEN = os.environ("GH_TOKEN")
+
+AUTH_TOKEN = os.environ("secrets.GH_TOKEN")
 
 SOURCE_BRANCH = args.head_branch
+#AUTH_TOKEN = args.auth_token
 
 api_url = " https://api.github.com/repos/OWNER/REPO/"
 
