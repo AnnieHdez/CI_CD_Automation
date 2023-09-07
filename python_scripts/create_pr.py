5eaf426eb9032a2b08a609b27f3c027706c31606
Annie Hernandez Perez <anniehperez@gmail.com>
2023-09-07 18:02:37 -0500

    doing request and using token

--- python_scripts/create_pr.py
+++ python_scripts/create_pr.py
@@ -1,6 +1,7 @@
 import json
 import requests
 import argparse
+import os
 
 # Initialize the Parser
 parser = argparse.ArgumentParser()
@@ -8,9 +9,27 @@ parser = argparse.ArgumentParser()
 # Adding Arguments
 parser.add_argument('--head_branch', type= str)
 
-
 args = parser.parse_args()
+AUTH_TOKEN = os.env("GH_TOKEN")
 
 SOURCE_BRANCH = args.head_branch
 
-print(SOURCE_BRANCH)
+api_url = " https://api.github.com/repos/OWNER/REPO/"
+
+api_action = "commits"
+
+url = api_url + api_action
+
+
+headers = {
+    "Accept": "application/vnd.github+json",
+    "Authorization": f"token {AUTH_TOKEN}"
+    }
+
+response = requests.get(
+    url=url,
+    headers=headers
+)
+
+if response.status_code==200:
+    print(json.dumps(response.json(),indent=4))
