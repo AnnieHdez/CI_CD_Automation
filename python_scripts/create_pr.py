703e2e1b8d599bb692821d90ecdd90d25a66a4c1
Annie Hernandez Perez <anniehperez@gmail.com>
2023-09-11 17:10:01 -0500

    refactoring request

--- python_scripts/create_pr.py
+++ python_scripts/create_pr.py
@@ -58,6 +58,8 @@ response = requests.request(
     data=json.dumps(params)
 )
 
+print(response.status_code)
+
 if response.status_code==201:
     print(json.dumps(response.json(),indent=4))
     # print("source:",SOURCE_BRANCH)
