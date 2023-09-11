a3714472d60465fa0c81481281e1703e89429345
Annie Hernandez Perez <anniehperez@gmail.com>
2023-09-11 16:55:26 -0500

    debug automatic pull request

--- python_scripts/create_pr.py
+++ python_scripts/create_pr.py
@@ -54,7 +54,7 @@ response = requests.post(
     }
 )
 
-if response==201:
+if response.status_code==201:
     print(json.dumps(response.json(),indent=4))
     # print("source:",SOURCE_BRANCH)
     # print("target:",TARGET_BRANCH)
